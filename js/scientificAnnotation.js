7e8be95262ac5eac31126a859b5b749efbb3dcaf
saifulnipo <saiful.nipo@gmail.com>
2013-11-27 09:32:01 +0100

    add new file for annotaion lab project

---
+++ js/scientificAnnotation.js
@@ -0,0 +1,123 @@
+/**
+ * Created with JetBrains PhpStorm.
+ * User: Islam_s
+ * Date: 21.11.13
+ * Time: 11:32
+ * To change this template use File | Settings | File Templates.
+ */
+
+
+var scientificAnnotation  = {
+
+
+    /**
+     * bind the click event
+     */
+    bindClickEventForAddAnnotationButton: function () {
+
+        $("#addAnnotationBtn").bind("click", function () {
+            //scientificAnnotation.displayAnnotationInputArea();
+            scientificAnnotation.addAnnotation();
+        });
+
+    },
+
+    /**
+     * bind the mouse up event
+     */
+    bindMouseUpEventForPDFViewer: function () {
+
+        $("#viewer").bind("mouseup", function () {
+
+            var text=scientificAnnotation.getSelectedTextFromPDF();
+            if (text!='') {
+                scientificAnnotation.setTextValue(text);
+            }
+        });
+
+    },
+
+
+    /**
+     * clear the values of input text field
+     */
+    clearInputField:function (){
+        $('#propertyValueInput').val('');
+        $('#objectValueInput').val('');
+    },
+
+
+    /**
+     * set the input
+     * @param selectedText
+     */
+    setTextValue:function(selectedText) {
+
+        selectedText =selectedText.replace(/(\r\n|\n|\r)/gm,"");
+        $('#objectValueInput').val(selectedText);
+    },
+
+    /**
+     * Get the selected text form pdf doc
+     * @returns {string}
+     */
+    getSelectedTextFromPDF : function(){
+
+        if (window.getSelection) {
+            return window.getSelection().toString();
+        } else if (document.selection) {
+            return document.selection.createRange().text;
+        }
+        return '';
+    },
+
+
+    /**
+     * perform the adding of  annotation
+     */
+    addAnnotation:function(){
+
+       var propertyValue = $('#propertyValueInput').val();
+       var objectValue = $('#objectValueInput').val();
+
+       if(propertyValue != '' && objectValue!= '') {
+           scientificAnnotation.appendAnnotationInDisplayPanel(propertyValue,objectValue);
+           scientificAnnotation.clearInputField();
+       }
+
+    },
+
+    /**
+     * Show the added annotation of the document
+     * @param propertyValue
+     * @param objectiveValue
+     */
+    appendAnnotationInDisplayPanel : function (propertyValue, objectiveValue){
+
+        var previousHtml = $('#displayAnnotationResult').html();
+        $('#displayAnnotationResult').empty();
+        $('#displayAnnotationResult').append(
+                '<p><strong>Property:</strong></br>'+propertyValue+'</p>' +
+                '<p><strong>Object:</strong></br>'+objectiveValue+'</p></br>'+
+                 previousHtml
+        );
+    },
+
+
+    /**
+     * Initialize the document
+     */
+    init:function(){
+        scientificAnnotation.bindClickEventForAddAnnotationButton();
+        scientificAnnotation.bindMouseUpEventForPDFViewer();
+    }
+
+};
+
+
+/**
+ * document on ready method
+ */
+$(function () {
+    scientificAnnotation.init();
+});
