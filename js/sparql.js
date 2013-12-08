8f6eeceae273c9ffe7ff9497ecd1972de60a9df3
saifulnipo <saiful.nipo@gmail.com>
2013-12-08 22:32:58 +0100

    add new file for sparql

---
+++ js/sparql.js
@@ -0,0 +1,30 @@
+/**
+ * Created with JetBrains PhpStorm.
+ * User: Islam_s
+ * Date: 21.11.13
+ * Time: 11:32
+ * To change this template use File | Settings | File Templates.
+ */
+
+
+var sparql  = {
+
+
+
+
+    /**
+     * Initialize the document
+     */
+    init:function(){
+
+    }
+
+};
+
+
+/**
+ * document on ready method
+ */
+$(function () {
+    sparql.init();
+});
